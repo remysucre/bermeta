44b4e8a1a7296430d8ca7cb402230d1bdaee8d24
remysucre <remy.sucre@gmail.com>
2020-03-23 13:50:43 -0700

    Add vector code.

---
+++ vector.ml
@@ -0,0 +1,52 @@
+type ('i, 'a) vec = Vec of 'i *
+                           ('i -> 'a)
+;;
+
+type ('i, 'a, 'w) ovec = OVec of 'i *
+                                 ('i -> 'a -> 'w)
+;;
+
+let vec_of_array: 'a array -> ((int, 'a) vec * (int, 'a, unit) ovec) = fun a ->
+  let n = Array.length a in
+  (Vec (n, fun i -> a.(i)),
+   OVec (n, fun i v -> a.(i) <- v)
+  )
+;;
+
+let vec_map : ('a -> 'b) -> ('i, 'a) vec -> ('i, 'b) vec =
+  fun tf (Vec (n, f)) -> Vec (n, fun i -> tf (f i))
+;;
+
+let zip_with : ('a -> 'b -> 'c) -> ('i, 'a) vec -> ('i, 'b) vec -> ('i, 'c) vec =
+  fun tf (Vec (n1, f1)) (Vec (n2, f2)) ->
+  Vec (n1, fun i -> tf (f1 i) (f2 i))
+;;
+
+let vec_assign : ('i, 'a, 'w) ovec -> ('i, 'a) vec -> ('i, 'w) vec =
+  fun (OVec (no, fo)) (Vec (ni, fi)) ->
+  Vec(no, fun i -> fo i (fi i))
+;;
+
+module type VEC = sig
+  type idx
+  type unt
+  val iter: (idx, unt) vec -> unt
+end
+
+module VecSta = struct
+  type idx = int
+  type unt = unit
+
+  let iter (Vec (n, body)) =
+    for i = 0 to n-1 do
+      body i
+    done
+end
+
+module BLAS1(R:RING)(A: VEC) = struct
+  open R
+  open A
+  let ( := ) out vin = iter (vec_assign vout vin)
+  let ( *. ) v1 v2 = zip_with mul v1 v2
+end
+
